3bd6c3213b52c95c327c110142efb1fef8dacd74
oversys <hani.alsayani11@gmail.com>
2025-04-23 12:43:23 +0200

    Add option to round temperatures and wind speed

--- hypr/scripts/weather.sh
+++ hypr/scripts/weather.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ROUND_TEMP=1
+ROUND_WIND=1
+
 # Automatically detect location
 while [[ -z "$LOC" ]]; do
 	LOC=$(curl -s https://ipinfo.io | jq -r '.loc')
@@ -40,33 +43,33 @@ weather_icon() {
 
 # Function to map wind direction to an arrow character
 wind_icon() {
-    local deg=$1
+	local deg=$1
 
-    if (( deg >= 0 && deg < 23 )) || (( deg >= 338 && deg <= 360 )); then
-        # echo ""  # N
+	if (( deg >= 0 && deg < 23 )) || (( deg >= 338 && deg <= 360 )); then
+		# echo ""  # N
 		echo ""
-    elif (( deg >= 23 && deg < 68 )); then
-        # echo ""  # NE
+	elif (( deg >= 23 && deg < 68 )); then
+		# echo ""  # NE
 		echo "↙"
-    elif (( deg >= 68 && deg < 113 )); then
-        # echo ""  # E
+	elif (( deg >= 68 && deg < 113 )); then
+		# echo ""  # E
 		echo ""
-    elif (( deg >= 113 && deg < 158 )); then
-        # echo ""  # SE
+	elif (( deg >= 113 && deg < 158 )); then
+		# echo ""  # SE
 		echo "↖"
-    elif (( deg >= 158 && deg < 203 )); then
-        # echo ""  # S
+	elif (( deg >= 158 && deg < 203 )); then
+		# echo ""  # S
 		echo ""
-    elif (( deg >= 203 && deg < 248 )); then
-        # echo ""  # SW
+	elif (( deg >= 203 && deg < 248 )); then
+		# echo ""  # SW
 		echo "↗"
-    elif (( deg >= 248 && deg < 293 )); then
-        # echo ""  # W
+	elif (( deg >= 248 && deg < 293 )); then
+		# echo ""  # W
 		echo ""
-    elif (( deg >= 293 && deg < 338 )); then
-        # echo ""  # NW
+	elif (( deg >= 293 && deg < 338 )); then
+		# echo ""  # NW
 		echo "↘"
-    fi
+	fi
 }
 
 ## Parse fields
@@ -83,10 +86,19 @@ real_feel=$(echo "$weather_info" | jq -r '.current.apparent_temperature')
 
 temp_icon=$(printf "%s%s%s" "<span font='18' rise='-3000'>" $(weather_icon "$weather_code") "</span>")
 
+if [[ "$ROUND_TEMP" -eq 1 ]]; then
+	temp=$(printf "%.0f" "$temp")
+	real_feel=$(printf "%.0f" "$real_feel")
+fi
+
 # Wind
 wind_speed=$(echo "$weather_info" | jq -r '.current.wind_speed_10m')
 wind_dir=$(echo "$weather_info" | jq -r '.current.wind_direction_10m')
 wind_dir_icon=$(printf "%s%s%s" "<span font='16' rise='-3000'>" $(wind_icon "$wind_dir") "</span>")
+
+if [[ "$ROUND_WIND" -eq 1 ]]; then
+	wind_speed=$(printf "%.0f" "$wind_speed")
+fi
 
 ## Print module json
 printf "{\"text\": \"$temp_icon $temp$temp_unit\", \"alt\": \"$temp_icon $temp$temp_unit ($real_feel$temp_unit)\", \"tooltip\": \"$wind_dir_icon $wind_speed $wind_speed_unit @ $wind_dir$wind_dir_unit\" }\n"
