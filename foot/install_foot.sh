7e2d00acadedebe722e26de7fce937ba99f90334
oversys <hani.alsayani11@gmail.com>
2026-05-23 01:06:36 +0200

    Add script to install foot ligature fork

---
+++ foot/install_foot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install dependencies
+sudo pacman -S meson ninja wayland-protocols libxkbcommon pixman tllist
+
+# Clone repo (latest commit only)
+git clone --depth 1 https://codeberg.org/barsmonster/foot
+cd foot
+
+# Edit the fcft subproject to use the ligature-enabled fork
+cat > subprojects/fcft.wrap << 'EOF'
+[wrap-git]
+url = https://codeberg.org/barsmonster/fcft
+revision = master
+depth = 1
+EOF
+
+# Build
+meson setup build --buildtype=release
+ninja -C build
+
+# Install
+sudo ninja -C build install
+
+# Uninstall
+# sudo ninja -C build uninstall
+
